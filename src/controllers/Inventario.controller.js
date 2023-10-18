92566334b0bd800aaf01c6f09b57a40f5e950d03
mauricioMatuz <killashe96@gmail.com>
2023-10-18 17:16:30 -0600

    Script para crear BD e inventario echo

--- src/controllers/Inventario.controller.js
+++ src/controllers/Inventario.controller.js
@@ -6,44 +6,45 @@ import { Inventario } from '../models/Inventario.model.js';
 
 export const ListarInventario = async (req, res) => {
   try {
-    const inventario = await Producto.findAll({
-      attributes: ['id', 'producto', 'descripcion'],
-      include: [
-        {
-          model: Entrada,
-          attributes: [
-            [sequelize.fn('SUM', sequelize.col('cantidad')), 'total_entradas'],
-          ],
-        },
-        {
-          model: Salida,
-          attributes: [
-            [sequelize.fn('SUM', sequelize.col('cantidad')), 'total_salidas'],
-          ],
-        },
-      ],
-      group: ['Producto.id'],
+    const productos = await Producto.findAll();
+
+    for (let index = 0; index < productos.length; index++) {
+      const producto = productos[index];
+      const entrada = await Entrada.findOne({
+        where: { productoId: producto.id },
+      });
+      const salida = await Salida.findOne({
+        where: { productoId: producto.id },
+      });
+
+      const cantidadEntrada = entrada ? entrada.dataValues.cantidad : 0;
+      const cantidadSalida = salida ? salida.dataValues.cantidad : 0;
+      const cantidadActual = cantidadEntrada - cantidadSalida;
+
+      // Busca un registro de Inventario existente para el producto
+      let inventario = await Inventario.findOne({
+        where: { productoId: producto.id },
+      });
+
+      // Si no existe un registro de Inventario, crea uno
+      if (!inventario) {
+        inventario = await Inventario.create({
+          productoId: producto.id,
+          cantidad_actual: cantidadActual,
+        });
+      } else {
+        // Si el registro de Inventario ya existe, actualiza la cantidad_actual
+        inventario.cantidad_actual = cantidadActual;
+        await inventario.save();
+      }
+    }
+
+    // Consulta el inventario nuevamente para obtener la información completa
+    const inventarioCompleto = await Producto.findAll({
+      include: [{ model: Inventario }],
     });
 
-    console.log(inventario, ' que es esto xd');
-    // const cantidad_actual = inventario.map((producto) => {
-    //   const totalEntradas = producto.Entrada
-    //     ? producto.Entrada[0].dataValues.total_entradas
-    //     : 0;
-    //   const totalSalidas = producto.Salida
-    //     ? producto.Salida[0].dataValues.total_salidas
-    //     : 0;
-    //   const cantidadActual = totalEntradas - totalSalidas;
-
-    //   return {
-    //     id: producto.id,
-    //     producto: producto.producto,
-    //     descripcion: producto.descripcion,
-    //     cantidad_actual: cantidadActual,
-    //   };
-    // });
-    const inventarios = await Inventario.create({ cantidad_actual });
-    return res.status(200).json(cantidad_actual);
+    return res.status(200).json(inventarioCompleto);
   } catch (error) {
     console.log(error);
     return res.status(500).json({ message: 'Error del servidor' });
--- src/database/database.js
+++ src/database/database.js
@@ -3,5 +3,5 @@ import Sequelize from 'sequelize';
 export const sequelize = new Sequelize('almacen', 'postgres', '123', {
   host: 'localhost',
   dialect: 'postgres',
-  // logging: false,
+  logging: false,
 });
